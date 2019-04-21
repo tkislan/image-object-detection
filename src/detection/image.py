3e5c6593efd909485770e6fd19745e279e0a2c55
Tomas Kislan <kislan.tomas@gmail.com>
2019-04-21 19:26:17 +0200

    Speedup image conversion to numpy, revert detection threshold

--- src/detection/image.py
+++ src/detection/image.py
@@ -2,13 +2,6 @@ import numpy as np
 from PIL import Image
 
 
-def convert_image_to_array(image):
-    (im_width, im_height) = image.size
-    return np.array(image.getdata()) \
-        .reshape((im_height, im_width, 3)) \
-        .astype(np.uint8)
-
-
 def load_image(file_path: str) -> np.array:
     print('Opening image')
     image = Image.open(file_path)
@@ -16,7 +9,7 @@ def load_image(file_path: str) -> np.array:
     print('Converting image to array')
     # the array based representation of the image will be used later in order to prepare the
     # result image with boxes and labels on it.
-    image_np = convert_image_to_array(image)
+    image_np = np.array(image)
 
     return image_np
 
