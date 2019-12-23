30fc5097f2434d6b12fb3651a92dde7c3fe59bf8
Tomas Kislan <kislan.tomas@gmail.com>
2019-12-23 08:58:46 +0100

    Fix threshold config variable

--- image_object_detection/detection/config.py
+++ image_object_detection/detection/config.py
@@ -2,4 +2,4 @@ import os
 
 
 DETECTION_CLASSES = os.environ.get('DETECTION_CLASSES', 'person').split(',')
-DETECTION_THRESHOLD = int(os.environ.get('DETECTION_THRESHOLD') or 0.5)
+DETECTION_THRESHOLD = float(os.environ.get('DETECTION_THRESHOLD') or 0.5)
